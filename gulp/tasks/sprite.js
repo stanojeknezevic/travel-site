75fcd6781e9eaf0d47110296c301bb1e8432d032
canek <sknest@gmail.com>
2017-06-15 22:10:35 +0200

    Completed

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -13,32 +13,32 @@ var config = {
         }
       }
     }
-  }
+  } 
 }
 
-gulp.task('beginClean', function() {
-  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+gulp.task('beginClean', function(){
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
-gulp.task('createSprite', ['beginClean'], function() {
+gulp.task('createSprite', ['beginClean'], function(){
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function() {
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+  return fulp.src('./app/temp/sprite/css/**/*.svg')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
-gulp.task('copySpriteCSS', ['createSprite'], function() {
+gulp.task('copySpriteCSS', ['createSprite'], function(){
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
-  return del('./app/temp/sprite');
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
+	return del('./app/temp/sprite');
 });
 
 gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,9 @@
-var gulp  = require('gulp');
-var watch = require('gulp-watch');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
+
   browserSync.init({
     notify: false,
     server: {
@@ -10,17 +11,25 @@ gulp.task('watch', function(){
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function(){
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+
 });
 
-
-gulp.task('cssInject', ['styles'], function(){
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
